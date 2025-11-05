52924da039cf5721bdd11242f78c376bcd0273c3
prutha-06 <rasalpratiksha0@gmail.com>
2025-11-06 00:09:32 +0530

    Added Fibonacci.js program

---
+++ Fibonacci.js
@@ -0,0 +1,11 @@
+let n = 10; // number of terms
+let a = 0, b = 1;
+
+console.log("Fibonacci Series:");
+
+for (let i = 0; i < n; i++) {
+  console.log(a);
+  let next = a + b;
+  a = b;
+  b = next;
+}
