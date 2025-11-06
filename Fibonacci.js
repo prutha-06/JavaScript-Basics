22d7e2547a09abf6578e7433c7e417d90067d0d2
prutha-06 <rasalpratiksha0@gmail.com>
2025-11-06 22:56:45 +0530

    prime number

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,11 +1,11 @@
-let n = 10; // number of terms
-let a = 0, b = 1;
+// let n = 10; // number of terms
+// let a = 0, b = 1;
 
-console.log("Fibonacci Series:");
+// console.log("Fibonacci Series:");
 
-for (let i = 0; i < n; i++) {
-  console.log(a);
-  let next = a + b;
-  a = b;
-  b = next;
-}
+// for (let i = 0; i < n; i++) {
+//   console.log(a);
+//   let next = a + b;
+//   a = b;
+//   b = next;
+// }
---
+++ primeNumber.js
@@ -0,0 +1,15 @@
+function isPrime(num){
+    if(num<=1) return false;
+    for(let i=2; i<Math.sqrt(num);i++){
+        if(num %i===0)
+            return false;
+    }
+    return true;
+}
+console.log(isPrime(7));
+console.log(isPrime(10));
+
+console.log("******************");
+
+let a=10;
+console.log(a);
